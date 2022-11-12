bd3ed2c20c034beb92f4e6d4345ac22fa0f1fef0
Andrey E. Dalosto <103847726+ADalosto@users.noreply.github.com>
2022-11-12 09:54:13 -0300

    Delete demoAndrey/demoAndrey directory

--- demoAndrey/demoAndrey/src/main/java/com/example/demoAndrey/repository/AlunoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.demoAndrey.repository;
-
-import com.example.demoAndrey.model.Aluno;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AlunoRepository extends JpaRepository <Aluno,Integer> {
-}
